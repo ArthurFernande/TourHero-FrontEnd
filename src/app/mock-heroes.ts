bdc91a54446fccaebe6e270ffd74191181817953
Arthur Fernande <arthur.andrade@ccc.ufcg.edu.br>
2024-02-13 17:15:20 -0300

    lista de herois

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,13 @@
+import { Hero } from "./hero.model";
+
+ export const HEROES: Hero[] =[
+  {id: 1, name:'Spiderman'},
+  {id: 2, name:'Superman'},
+  {id: 3, name:'Batman'},
+  {id: 4, name:'Flash'},
+  {id: 5, name:'Sonic'},
+  {id: 6, name:'Aquaman'},
+  {id: 7, name:'Pantera Negra'},
+  {id: 8, name:'Arqueiro verde'},
+  {id: 9, name:'Mulher Maravilha'},
+];
